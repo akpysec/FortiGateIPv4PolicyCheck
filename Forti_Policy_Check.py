cf0bd56b359bfdd0e12224a12a24f23b1579c383
andreykbjj <48283299+andreykbjj@users.noreply.github.com>
2020-07-03 11:01:50 +0300

    Update Forti_Policy_Check.py

--- Forti_Policy_Check.py
+++ Forti_Policy_Check.py
@@ -32,7 +32,11 @@ def all_objects_sum(every_rule: list):
                'set service "ALL"',
                'edit',
                'set action accept',
-               'set status disable']
+               'set status disable',
+               '"SMB"',
+               '"FTP"',
+               '"TELNET"',
+]
 
     srcdstsrv_list = list()
     srcdst_list = list()
@@ -43,16 +47,26 @@ def all_objects_sum(every_rule: list):
     srv_list = list()
 
     disabled_rules_list = list()
-    
+
+    smb_list = list()
+    ftp_list = list()
+    telnet_list = list()
+
     for l in sorted(every_rule):
         if objects[3] in l:
+            
+            # Assign variable to rule ID's
             rule_num = l.split(" ")[1].rstrip("\nset")
+
+            # Check for "All" object in Source, Destination and Service fields
             if l.__contains__(objects[0]) and l.__contains__(objects[1]) and l.__contains__(objects[2]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     srcdstsrv_list.append(rule_num)
                     # with open(f"All object in Source, Destination and Service fields.txt", "a") as srcdstsrv:
                     #     srcdstsrv.writelines(l)
                         # print("edit", rule_num)
+
+            # Check for "All" object in Source and Destination fields
             if l.__contains__(objects[0]) and l.__contains__(objects[1]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     if not l.__contains__(objects[2]):
@@ -60,6 +74,8 @@ def all_objects_sum(every_rule: list):
                         # with open("All object in Source & Destination fields.txt", "a") as srcdst:
                         # srcdst.writelines(l)
                         # print(rule_num)
+
+            # Check for "All" object in Source and Service fields
             if l.__contains__(objects[0]) and  l.__contains__(objects[2]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     if not l.__contains__(objects[1]):
@@ -67,6 +83,8 @@ def all_objects_sum(every_rule: list):
                         # with open("All object in Source and Service fields.txt", "a") as srcsrv:
                             # srcsrv.writelines(l)
                             # print(rule_num)
+
+            # Check for "All" object in Destination and Service fields
             if l.__contains__(objects[1]) and l.__contains__(objects[2]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     if not l.__contains__(objects[0]):
@@ -74,6 +92,8 @@ def all_objects_sum(every_rule: list):
                         # with open("All object in Destination and Service fields.txt", "a") as dstsrv:
                             # dstsrv.writelines(l)
                             # print(rule_num)
+
+            # Check for "All" object in Destination field
             if l.__contains__(objects[1]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     if not l.__contains__(objects[0]):
@@ -82,6 +102,8 @@ def all_objects_sum(every_rule: list):
                             # with open("All object in Destination field.txt", "a") as dst:
                                 # dst.writelines(l)
                                 # print(rule_num)
+
+            # Check for "All" object in Source field
             if l.__contains__(objects[0]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     if not l.__contains__(objects[1]):
@@ -90,6 +112,8 @@ def all_objects_sum(every_rule: list):
                             # with open("All object in Source field.txt", "a") as src:
                                 # src.writelines(l)
                                 # print(rule_num)
+
+            # Check for "All" object in Service field
             if l.__contains__(objects[2]) and l.__contains__(objects[4]):
                 if not l.__contains__(objects[5]):
                     if not l.__contains__(objects[0]):
@@ -99,19 +123,29 @@ def all_objects_sum(every_rule: list):
                                 # srv.writelines(l)
                                 # print(rule_num)
 
+            # Check for Disabled policies                    
             if l.__contains__(objects[5]):
                 disabled_rules_list.append(rule_num)
                 # with open("Disabled rules.txt", "a") as dis:
                 #     dis.writelines(l)
+                
+            # Check for potentially Un-safe protocols
+            if l.__contains__(objects[6]):
+                smb_list.append(rule_num)
+                # with open("Un-safe protocols.txt", "a") as un_safe:
+                #     un_safe.writelines(l)
 
-    with open('Summary.txt', 'a') as summary:
-        # print(f"All object in Source, Destination & Service fields\nRules in total: {len(srcdstsrv_list)}\nRules numbers: {srcdstsrv_list}\n")
-        # print(f"All object in Source & Destination fields\nRules in total: {len(srcdst_list)}\nRules numbers: {srcdst_list}\n")
-        # print(f"All object in Source & Service fields\nRules in total: {len(srcsrv_list)}\nRules numbers: {srcsrv_list}\n")
-        # print(f"All object in Destination & Service fields\nRules in total: {len(dstsrv_list)}\nRules numbers: {dstsrv_list}\n")
-        # print(f"All object in Source field\nRules in total: {len(src_list)}\nRules numbers: {src_list}\n")
-        # print(f"All object in Destination field\nRules in total: {len(dst_list)}\nRules numbers: {dst_list}\n")
-        # print(f"All object in Service field\nRules in total: {len(srv_list)}\nRules numbers: {srv_list}\n")
+            if l.__contains__(objects[7]):
+                ftp_list.append(rule_num)
+                # with open("Un-safe protocols.txt", "a") as un_safe:
+                #     un_safe.writelines(l)
+
+            if l.__contains__(objects[8]):
+                telnet_list.append(rule_num)
+                # with open("Un-safe protocols.txt", "a") as un_safe:
+                #     un_safe.writelines(l)
+
+    with open('Summary.txt', 'a') as summary:        
         summary.write(f"All object in Source, Destination & Service fields\nRules in total: {len(srcdstsrv_list)}\nRules ID's: {srcdstsrv_list}\n\n")
         summary.write(f"All object in Source & Destination fields\nRules in total: {len(srcdst_list)}\nRules ID's: {srcdst_list}\n\n")
         summary.write(f"All object in Source & Service fields\nRules in total: {len(srcsrv_list)}\nRules ID's: {srcsrv_list}\n\n")
@@ -120,7 +154,9 @@ def all_objects_sum(every_rule: list):
         summary.write(f"All object in Destination field\nRules in total: {len(dst_list)}\nRules ID's: {dst_list}\n\n")
         summary.write(f"All object in Service field\nRules in total: {len(srv_list)}\nRules ID's: {srv_list}\n\n")
         summary.write(f"Disabled Rules\nRules in total: {len(disabled_rules_list)}\nRules ID's: {disabled_rules_list}\n\n")
+        summary.write(f"SMB presents in rules\nRules in total: {len(smb_list)}\nRules ID's: {smb_list}\n\n")
+        summary.write(f"FTP presents in rules\nRules in total: {len(ftp_list)}\nRules ID's: {ftp_list}\n\n")
+        summary.write(f"TELNET presents in rules\nRules in total: {len(telnet_list)}\nRules ID's: {telnet_list}\n\n")
 
 
 all_objects_sum(every_rule=read_output_list(ipv4_policy="firewallPolicy.txt"))
-
